fb485948f4431fe62d321262c475bdac35a7f10c
Egor Bondar <Erop_96_15@mail.ru>
2022-01-06 22:27:05 +1000

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticketCost = 20000;
+        int rubPerMile = 20;
+
+        int bonus = ticketCost / rubPerMile;
+
+        System.out.println(" Накопленные мили " + bonus);
+
+
+    }
+}
